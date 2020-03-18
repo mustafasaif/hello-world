e1d54f0298f3a75a760775d776b7b164d770c0ff
mustafasaif <57667648+mustafasaif@users.noreply.github.com>
2020-03-19 01:17:20 +0800

    Add files via upload

---
+++ CW1.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+m_path_1=[]
+m_path_2=[]
+
+def convert(val):
+    if val.endswith('Kb'):
+        return float(val[:-2])/1000
+    elif val.endswith('Mb'):
+        return float(val[:-2])
+    elif val.endswith('b'):
+        return float(val[:-1])/1000000
+    else:
+        return 0
+
+with open("network_analysis_data.txt", "r") as file:
+    count=0
+    for line in file:
+        count+= 1
+        if count%2== 0:
+            line1=line.split()[8]
+            m_path_2.append(convert(line1))
+        else:
+            line2=line.split()[8]
+            m_path_1.append(convert(line2))
+plt.figure(1)
+plt.xlabel('Data Row')
+plt.ylabel('Data Rate[Mb/s]')
+plt.plot(m_path_1,'blue', label='Path 1')
+plt.plot(m_path_2,'red', label='Path 2')
+plt.title('Path 1&2')
+plt.legend()
+plt.grid()
+
+plt.figure(2)
+plt.plot(m_path_1,'blue', label='Path 1')
+plt.xlabel('Data Row')
+plt.ylabel('Data Rate[Mb/s]')
+plt.title('Path 1')
+plt.grid()
+plt.legend()
+
+plt.figure(3)
+plt.plot(m_path_2,'red', label='Path 2')
+plt.xlabel('Data Row')
+plt.ylabel('Data Rate[Mb/s]')
+plt.title('Path 2')
+plt.legend()
+plt.grid()
+plt.show()
